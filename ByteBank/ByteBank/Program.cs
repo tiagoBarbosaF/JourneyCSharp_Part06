77b0785be169162f3feff8b2219a02e61b6b477c
Tiago Barbosa <tiago@farias.cc>
2021-10-28 17:55:47 -0300

    Journey CSharp Part06 - Strings, regular expressions and Object class

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            string url = "pagina?argumentos";
+
+            //Console.WriteLine(argumentos);
+
+            int indiceInterrogacao = url.IndexOf('?');
+            string argumentos = url.Substring( indiceInterrogacao + 1 );
+            Console.WriteLine( argumentos );
+
+            Console.ReadLine();
+        }
+    }
+}
